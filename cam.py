9515d7b3ab1756174893d8ca88b4f4eaab6dbd55
Anindyadeep <2005641@kiit.ac.in>
2022-05-10 17:53:31 +0530

    pl

--- cam.py
+++ cam.py
@@ -14,33 +14,33 @@ if on:
     cap = cv2.VideoCapture(cam_source)
     app_window = st.image([])
 
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
-    FLAG = True 
-    size = (frame_width, frame_height)
+    # frame_width = int(cap.get(3))
+    # frame_height = int(cap.get(4))
+    # FLAG = True 
+    # size = (frame_width, frame_height)
 
-if on:   
-    result = cv2.VideoWriter('./pp.avi', 
-                            cv2.VideoWriter_fourcc(*'MJPG'),
-                            10, size)
+# if on:   
+#     result = cv2.VideoWriter('./pp.avi', 
+#                             cv2.VideoWriter_fourcc(*'MJPG'),
+#                             10, size)
 
     while on and not off:
         ret, frame = cap.read()
         if ret:
-            result.write(frame)
+            # result.write(frame)
             app_window.image(frame)
             cv2.waitKey(30)
 
         if off:
             break 
     cap.release()
-    result.release()
+    # result.release()
 
-if Flag and off:
-    with open("pp.avi", "rb") as file:
-        btn = st.download_button(
-            'Download',
-            data = file, 
-            file_name= "pp.avi",
-            mime="video/avi"
-        )
+# if Flag and off:
+#     with open("pp.avi", "rb") as file:
+#         btn = st.download_button(
+#             'Download',
+#             data = file, 
+#             file_name= "pp.avi",
+#             mime="video/avi"
+#         )
